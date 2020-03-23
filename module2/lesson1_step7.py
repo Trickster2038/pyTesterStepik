b320d826fe41bf7bbc0c1e25e9b3c209570431ad
Desktop_home <fzastahov@gmail.com>
2020-03-23 08:34:22 +0300

    module21

---
+++ module2/lesson1_step7.py
@@ -0,0 +1,40 @@
+import math
+from selenium import webdriver
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+
+	link = "http://suninjuly.github.io/get_attribute.html"
+	browser = webdriver.Chrome()
+	browser.get(link)
+
+	treasure = browser.find_element_by_css_selector("#treasure")
+	x = treasure.get_attribute("valuex")
+	#x_element = browser.find_element_by_css_selector("#input_value")
+	#x = x_element.text
+	y = calc(x)
+
+	b_element = browser.find_element_by_css_selector("#answer")
+	b_element.send_keys(str(y))
+
+	check = browser.find_element_by_css_selector("#robotCheckbox")
+	radio = browser.find_element_by_css_selector("#robotsRule")
+	check.click()
+	radio.click()
+
+	treasure = browser.find_element_by_css_selector("#robotsRule")
+
+	time.sleep(1)
+
+	button = browser.find_element_by_css_selector("""[text()="Submit"]""")
+   # button.click()
+
+finally:
+
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
