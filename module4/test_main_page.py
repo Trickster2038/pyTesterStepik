af4daca64420d0ac4b8698b797e9d0e5c04ab4d4
Desktop_home <fzastahov@gmail.com>
2020-04-12 21:11:16 +0300

    object_page_nice_exceptions

---
+++ module4/test_main_page.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+from .pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.should_be_login_link()      # выполняем метод страницы - переходим на страницу логина
