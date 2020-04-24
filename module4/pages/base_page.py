61b3f893cdc93ef2cc64000c937ae3d364bb5f8b
Desktop_home <fzastahov@gmail.com>
2020-04-25 01:54:27 +0300

    m4_assert_fixed

--- module4/pages/base_page.py
+++ module4/pages/base_page.py
@@ -1,4 +1,5 @@
 import pytest
+from selenium.common.exceptions import NoAlertPresentException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
--- module4/pages/product_page.py
+++ module4/pages/product_page.py
@@ -2,19 +2,32 @@ from .base_page import BasePage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .locators import ProductPageLocators
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.common import exceptions 
 
 class ProductPage(BasePage):
     def find_code(self):
+        self.browser.implicitly_wait(10)
         button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
         name = self.browser.find_element(*ProductPageLocators.NAME)
         cost = self.browser.find_element(*ProductPageLocators.COST)
+        namet = name.text
+        costt = cost.text
+        print("<name>")
+        print(namet)
+        print("</name>")
+        print(costt)
         button.click()
         self.solve_quiz_and_get_code()
         cost1 = self.browser.find_element(*ProductPageLocators.COST1)
         name1 = self.browser.find_element(*ProductPageLocators.NAME1)
-        #print(cost.text)
-        print(cost1.text)
-        #print(name.text)
+        print("<name1>")
         print(name1.text)
-        #assert name.text in name1.text, "Name notification bug"
-        #assert cost.text in cost1.text, "Cost notification bug"
+        print("</name1>")
+        print(cost1.text)
+        #print(cost.text)
+        
+        
+        assert f"{namet} был добавлен в вашу корзину." in name1.text, "Name notification bug"
+        assert costt in cost1.text, "Cost notification bug"
